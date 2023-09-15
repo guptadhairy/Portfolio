54edb92227d48030251b0d87309933e218a8cf13
guptadhairy <21it3032@rgipt.ac.in>
2023-09-15 18:25:23 +0530

    Ye last baar hai

--- src/app/Components/AboutContent.jsx
+++ src/app/Components/AboutContent.jsx
@@ -5,8 +5,8 @@ const AboutContent = () => {
     <div>
       <div className="meleft">
         <h1>About Me</h1>
-        <p>Hi, I'm <b>Dhairya Gupta</b>, a Full-Stack web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. With 2 years of experience in the field. I am always looking for new and innovative ways to bring my clients' visions to life.</p>
-        <p>I believe that design is about more than just making things look pretty it's about solving problems and creating intuitive, enjoyable experiences for users.</p>
+        <p>Hi Im <b>Dhairya Gupta</b> a Full-Stack web developer and UI/UX designer with a passion for creating beautiful functional and user-centered digital experiences. With 2 years of experience in the field. I am always looking for new and innovative ways to bring my clients visions to life.</p>
+        <p>I believe that design is about more than just making things look pretty its about solving problems and creating intuitive enjoyable experiences for users</p>
       </div>
       <div className="meImage">
           <Image className="meimg" width={400} height={300} src="/gallery.png" alt="profile" />
